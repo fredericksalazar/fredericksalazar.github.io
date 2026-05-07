d82815f928d2c01fd51b735a97ecd7eb3e25f154
FrederickSalazar <fredefass01@gmail.com>
2026-05-07 14:42:05 -0500

    ajuste hero observatorio

--- src/lib/observatorio/data.ts
+++ src/lib/observatorio/data.ts
@@ -6,6 +6,3 @@ const obs = raw as unknown as ObservatorioData;
 export const getObservatorio = (): ObservatorioData => obs;
 
 export const getSerieAscending = (): SerieFila[] => [...obs.serie].reverse();
-
-export const getCommitUrl = (): string =>
-  "https://github.com/fredericksalazar/fredericksalazar.github.io/commits/main/src/data/observatorio.json";
