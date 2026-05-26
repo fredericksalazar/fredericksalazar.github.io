a52d1bd17795e59b5d19e0fe606aade24771660e
FrederickSalazar <fredefass01@gmail.com>
2026-05-26 09:44:51 -0500

    fix problemas urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fredericksalazar.github.io',
+  trailingSlash: 'always',
   prefetch: {
     prefetchAll: false,
     defaultStrategy: 'hover',
